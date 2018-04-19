92498b55c3ce55650373e7f353a3b8c476239020
Managersky <witoldmetel@gmail.com>
2018-04-19 13:01:34 +0200

    Redux - basics

---
+++ Redux_Training/src/containers/book-list.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+class BookList extends React.Component {
+    renderList() {
+        return this.props.books.map((book) => {
+            return (
+                <li
+                    onClick={() => this.props.selectBook(book)}
+                    key={book.title}
+                    className="list-group-item">{book.title}
+                </li>
+            );
+        });
+    }
+
+    render() {
+        return (
+            <ul className="list-group col-sm-4">
+                {this.renderList()}
+            </ul>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    // props inside BookList
+    return {
+        books: state.books
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
---
+++ Redux_Training/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'JS some', pages: 101 },
+        { title: 'Ajax some', pages: 33 },
+        { title: 'PHP some', pages: 23 },
+        { title: 'CSS some', pages: 1 }
+    ]
+}
