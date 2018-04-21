c2d21853ebaf2911a2bc866f1c4a239b28743756
Managersky <witoldmetel@gmail.com>
2018-04-21 11:42:45 +0200

    add charts

--- Redux-Advanced-Training/src/components/app.js
+++ Redux-Advanced-Training/src/components/app.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/searchBar';
+import WeatherList from '../containers/weatherList';
 
 export default class App extends Component {
   render() {
     return (
-      <SearchBar />
+      <div>
+        <SearchBar />
+        <WeatherList />
+      </div>
     );
   }
 }
---
+++ Redux-Advanced-Training/src/containers/weatherList.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart';
+
+class weatherList extends Component {
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+        const temperature = _.map(cityData.list.map(weather => weather.main.temp), (temp) => (temp - 273.15)); //Convert Kelvin to Celcius
+        const pressure = cityData.list.map(weather => weather.main.pressure);
+        const humidity = cityData.list.map(weather => weather.main.humidity);
+
+        return (
+            <tr key={name}>
+                <td>{name}</td>
+                <td><Chart data={temperature} color="blue" units="°C"/></td>
+                <td><Chart data={pressure} color="green" units="hPa"/></td>
+                <td><Chart data={humidity} color="orange" units="%"/></td>
+            </tr>
+        );
+    }
+
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature [°C]</th>
+                        <th>Pressure [hPa]</th>
+                        <th>Humidity [%]</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        );
+    }
+}
+
+function mapStateToProps({ weather }) {
+    return { weather }
+}
+
+export default connect(mapStateToProps)(weatherList)
