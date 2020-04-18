26eee7b4e511b8e0e8055672889e3043e537ebb1
witoldmetel <witoldmetel@gmail.com>
2020-04-18 11:14:25 +0200

    Add state

--- 2020-Update/react-basics-2020-update/src/index.js
+++ 2020-Update/react-basics-2020-update/src/index.js
@@ -2,13 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+	constructor() {
+		super();
+
+		this.state = { lat: null, error: null };
+	}
+
 	render() {
 		window.navigator.geolocation.getCurrentPosition(
-			(position) => console.log(position),
-			(err) => console.log(err)
+			(position) => this.setState({ lat: position.coords.latitude }),
+			(err) => this.setState({ error: err.message })
 		);
 
-		return <div>Latitude: </div>;
+		if (!this.state.lat && !this.state.error) {
+			return <div>Loading...</div>;
+		} else {
+			return this.state.lat ? (
+				<div>Latitude: {this.state.lat}</div>
+			) : (
+				<div>Error: {this.state.error}</div>
+			);
+		}
 	}
 }
 
