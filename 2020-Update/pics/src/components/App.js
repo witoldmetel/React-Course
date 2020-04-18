7785d498794b2034ca956ba8e1e15d76e7fc40d9
witoldmetel <witoldmetel@gmail.com>
2020-04-18 16:16:51 +0200

    event handling and form

--- 2020-Update/pics/src/components/App.js
+++ 2020-Update/pics/src/components/App.js
@@ -3,10 +3,14 @@ import React from 'react';
 import { SearchBar } from '../components/SearchBar';
 
 export class App extends React.Component {
+	onFormSubmit = (term) => {
+		console.log(term);
+	};
+
 	render() {
 		return (
 			<div className="ui container" style={{ marginTop: '10px' }}>
-				<SearchBar />
+				<SearchBar onSubmit={this.onFormSubmit} />
 			</div>
 		);
 	}
--- 2020-Update/pics/src/components/SearchBar.js
+++ 2020-Update/pics/src/components/SearchBar.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
 export class SearchBar extends React.Component {
+	state = { inputValue: '' };
+
+	onFormSubmit = (e) => {
+		e.preventDefault();
+
+		this.props.onSubmit(this.state.inputValue);
+
+		this.setState({ inputValue: '' });
+	};
+
+	onInputChange = (e) => {
+		this.setState({ inputValue: e.target.value });
+	};
+
 	render() {
 		return (
 			<div className="ui segment">
-				<form className="ui form">
+				<form onSubmit={this.onFormSubmit} className="ui form">
 					<div className="field">
 						<label>Image Search</label>
-						<input type="text" />
+						<input
+							type="text"
+							value={this.state.inputValue}
+							onChange={this.onInputChange}
+						/>
 					</div>
 				</form>
 			</div>
