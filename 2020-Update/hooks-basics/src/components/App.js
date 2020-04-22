4c007f9a980174fc4202cebfbe0ef60b92527a5a
witoldmetel <witoldmetel@gmail.com>
2020-04-22 19:46:38 +0200

    Init

---
+++ 2020-Update/hooks-basics/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+import ResourceList from './ResourceList';
+
+const App = () => {
+	const [resource, setResource] = useState('posts');
+
+	return (
+		<div className="App">
+			<button onClick={() => setResource('posts')}>Posts</button>
+			<button onClick={() => setResource('todos')}>Todos</button>
+			<ResourceList resource={resource} />
+		</div>
+	);
+};
+
+export default App;
---
+++ 2020-Update/hooks-basics/src/components/ResourceList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ResourceList = (props) => {
+	return <div className="resource-list">{props.resource}</div>;
+};
+
+export default ResourceList;
