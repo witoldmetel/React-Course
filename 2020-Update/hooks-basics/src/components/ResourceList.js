46dc589d656c88d1d7186ce2758ca4fe927ea2ad
witoldmetel <witoldmetel@gmail.com>
2020-04-23 17:46:15 +0200

    useEffect

--- 2020-Update/hooks-basics/src/components/ResourceList.js
+++ 2020-Update/hooks-basics/src/components/ResourceList.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const ResourceList = (props) => {
-	return <div className="resource-list">{props.resource}</div>;
+const ResourceList = ({ resource }) => {
+	const [resources, setResources] = useState([]);
+
+	const fetchResource = async (resource) => {
+		const response = await axios.get(
+			`https://jsonplaceholder.typicode.com/${resource}`
+		);
+
+		setResources(response.data);
+	};
+
+	useEffect(() => {
+		fetchResource(resource);
+	}, [resource]);
+
+	return <div className="resource-list">{resources.length}</div>;
 };
 
 export default ResourceList;
