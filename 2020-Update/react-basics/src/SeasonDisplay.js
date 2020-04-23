65edbc90474d5482b231918c5a95c58ddede2125
witoldmetel <witoldmetel@gmail.com>
2020-04-23 18:01:42 +0200

    Add style

--- 2020-Update/react-basics/src/SeasonDisplay.js
+++ 2020-Update/react-basics/src/SeasonDisplay.js
@@ -2,29 +2,36 @@ import React from 'react';
 
 import { AcUnit, WbSunny } from '@material-ui/icons';
 
+import './SeasonDisplay.css';
+
+const seasonConfig = {
+	summer: {
+		text: "Let's hit the beach!",
+		icon: <WbSunny />,
+	},
+	winter: {
+		text: 'Burr it is cold!',
+		icon: <AcUnit />,
+	},
+};
+
+const getSeason = (lat, month) => {
+	if (month > 2 && month < 9) {
+		return lat > 0 ? 'summer' : 'winter';
+	} else {
+		return lat > 0 ? 'winter' : 'summer';
+	}
+};
+
 export const SeasonDisplay = (props) => {
-	const getSeason = (latitude) => {
-		const month = new Date().getMonth();
+	const season = getSeason(props.lat, new Date().getMonth());
+	const { text, icon } = seasonConfig[season];
 
-		if (month > 2 && month < 9) {
-			return latitude > 0 ? 'summer' : 'winter';
-		} else {
-			return latitude < 0 ? 'summer' : 'winter';
-		}
-	};
-
-	const getRenderer = () => {
-		return getSeason(props.lat) === 'summer' ? (
-			<div>
-				<WbSunny />
-				SUMMER
-			</div>
-		) : (
-			<div>
-				<AcUnit /> WINTER
-			</div>
-		);
-	};
-
-	return getRenderer();
+	return (
+		<div className={`season-display ${season}`}>
+			<i className={`icon-left massive icon`}>{icon}</i>
+			<h1>{text}</h1>
+			<i className={`icon-right massive icon`}>{icon}</i>
+		</div>
+	);
 };
