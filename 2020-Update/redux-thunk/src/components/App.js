4b771e5adc2b7d89135aa842d71d67bcbba1ac36
witoldmetel <witoldmetel@gmail.com>
2020-05-06 19:09:33 +0200

    init

---
+++ 2020-Update/redux-thunk/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { fetchPosts } from '../actions';
+
+class App extends React.Component {
+	componentDidMount() {
+		this.props.fetchPosts();
+	}
+
+	render() {
+		return <div className="App">App</div>;
+	}
+}
+
+export default connect(null, fetchPosts)(App);
