ce9a634ef0a825b024838e2669d7dd67d4f22587
witoldmetel <witoldmetel@gmail.com>
2020-05-19 18:08:28 +0200

    working redux

--- UPDATE_2020/streams/client/src/components/GoogleAuth.js
+++ UPDATE_2020/streams/client/src/components/GoogleAuth.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class GoogleAuth extends Component {
-	state = { isSignedIn: null };
+import { signIn, signOut } from '../actions';
 
+class GoogleAuth extends Component {
 	componentDidMount() {
 		window.gapi.load('client:auth2', () => {
 			window.gapi.client
@@ -13,27 +14,31 @@ export default class GoogleAuth extends Component {
 				})
 				.then(() => {
 					this.auth = window.gapi.auth2.getAuthInstance();
-					this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+					this.onAuthChange(this.auth.isSignedIn.get());
 					this.auth.isSignedIn.listen(this.onAuthChange);
 				});
 		});
 	}
 
-	onAuthChange = () =>
-		this.setState({ isSignedIn: this.auth.isSignedIn.get() });
-
+	onAuthChange = (isSignIn) => {
+		if (isSignIn) {
+			this.props.signIn();
+		} else {
+			this.props.signOut();
+		}
+	};
 	onSignInClick = () => this.auth.signIn();
 
 	onSignOutClick = () => this.auth.signOut();
 
 	get renderAuthButton() {
-		if (this.state.isSignedIn === null) {
+		if (this.props.isSignedIn === null) {
 			return (
 				<div className="ui active inverted dimmer">
 					<div className="ui loader"></div>
 				</div>
 			);
-		} else if (this.state.isSignedIn) {
+		} else if (this.props.isSignedIn) {
 			return (
 				<button className="ui red google button" onClick={this.onSignOutClick}>
 					<i className="google icon"></i>
@@ -54,3 +59,9 @@ export default class GoogleAuth extends Component {
 		return <div>{this.renderAuthButton}</div>;
 	}
 }
+
+const mapStateToProps = (state) => {
+	return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
--- UPDATE_2020/streams/client/src/reducers/index.js
+++ UPDATE_2020/streams/client/src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 
+import authReducer from './authReducer';
+
 export default combineReducers({
-	replace: () => true,
+	auth: authReducer,
 });
