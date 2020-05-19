67119b1f71b08ad675c45762814bd72a6994dce5
witoldmetel <witoldmetel@gmail.com>
2020-05-19 19:56:44 +0200

    Update create stream using POST method

--- UPDATE_2020/streams/client/src/components/streams/StreamCreate.js
+++ UPDATE_2020/streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+
+import { createStream } from '../../actions';
 
 class StreamCreate extends Component {
 	renderInput = ({ label, input, meta }) => {
@@ -17,8 +20,8 @@ class StreamCreate extends Component {
 		);
 	};
 
-	onSubmit = (e) => {
-		e.preventDefault();
+	onSubmit = (formValues) => {
+		this.props.createStream(formValues);
 	};
 
 	render() {
@@ -53,7 +56,9 @@ const validate = (formValues) => {
 	return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
 	form: 'streamCreate',
 	validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
