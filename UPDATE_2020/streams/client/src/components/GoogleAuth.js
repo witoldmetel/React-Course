adb028836d0dce4e30d693e0197b1b4f79241a9c
witoldmetel <witoldmetel@gmail.com>
2020-05-19 18:12:22 +0200

    Add userId state

--- UPDATE_2020/streams/client/src/components/GoogleAuth.js
+++ UPDATE_2020/streams/client/src/components/GoogleAuth.js
@@ -22,7 +22,7 @@ class GoogleAuth extends Component {
 
 	onAuthChange = (isSignIn) => {
 		if (isSignIn) {
-			this.props.signIn();
+			this.props.signIn(this.auth.currentUser.get().getId());
 		} else {
 			this.props.signOut();
 		}
