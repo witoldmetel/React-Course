0763700a95ac11784b72ecfe2ec0f55207266b22
witoldmetel <witoldmetel@gmail.com>
2020-05-20 16:37:00 +0200

    add lodash and update rest of reducers

--- UPDATE_2020/streams/client/src/reducers/streamReducer.js
+++ UPDATE_2020/streams/client/src/reducers/streamReducer.js
@@ -1,3 +1,5 @@
+import { omit, mapKeys } from 'lodash';
+
 import {
 	CREATE_STREAM,
 	FETCH_STREAMS,
@@ -9,16 +11,16 @@ import {
 export default (state = {}, action) => {
 	switch (action.type) {
 		case CREATE_STREAM:
-			return { ...state, [action.payload.id]: action.payload };
+			return { ...state, ...mapKeys(action.payload, 'id') };
 
 		case FETCH_STREAMS:
-			return {};
+			return { ...state, [action.payload.id]: action.payload };
 
 		case FETCH_STREAM:
 			return { ...state, [action.payload.id]: action.payload };
 
 		case DELETE_STREAM:
-			return {};
+			return omit(state, action.payload);
 
 		case EDIT_STREAM:
 			return { ...state, [action.payload.id]: action.payload };
