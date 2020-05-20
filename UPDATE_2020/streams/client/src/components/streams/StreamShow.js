2be2310748dcb4a6f00465d8aa6397c8f7616b06
witoldmetel <witoldmetel@gmail.com>
2020-05-20 21:17:00 +0200

    Update stream show

--- UPDATE_2020/streams/client/src/components/streams/StreamShow.js
+++ UPDATE_2020/streams/client/src/components/streams/StreamShow.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// const NodeMediaServer = require('node-media-server');
 
-export default class StreamShow extends Component {
+import { fetchStream } from '../../actions';
+
+class StreamShow extends Component {
+	componentDidMount() {
+		this.props.fetchStream(this.props.match.params.id);
+	}
+
 	render() {
-		return <div>StreamShow</div>;
+		return !this.props.stream ? (
+			<div className="ui active inverted dimmer">
+				<div className="ui loader"></div>
+			</div>
+		) : (
+			<div className="content">
+				<h2 className="header">{this.props.stream.title}</h2>
+				<div className="description">{this.props.stream.description}</div>
+			</div>
+		);
 	}
 }
+
+const mapStateToProps = (state, ownProps) => {
+	return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
