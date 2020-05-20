e0d98fc3d6a674a1761648339e46ac9c3ef187de
witoldmetel <witoldmetel@gmail.com>
2020-05-20 21:21:44 +0200

    Init rtmp server

--- UPDATE_2020/streams/client/src/components/streams/StreamShow.js
+++ UPDATE_2020/streams/client/src/components/streams/StreamShow.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// const NodeMediaServer = require('node-media-server');
 
 import { fetchStream } from '../../actions';
 
