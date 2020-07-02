f8743c2cb9f21ff682b01ce015fc57a85ebcad56
witoldmetel <witoldmetel@gmail.com>
2020-07-02 19:02:16 +0200

    update comment list

--- UPDATE_2020/testing/src/components/CommentList.js
+++ UPDATE_2020/testing/src/components/CommentList.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class CommentList extends Component {
+class CommentList extends Component {
 	render() {
-		return <div>CommentList</div>;
+		return (
+			<ul>
+				{this.props.comments.map((comment, index) => {
+					return <li key={index}>{comment}</li>;
+				})}
+			</ul>
+		);
 	}
 }
+
+const mapStateToProps = (state) => {
+	return {
+		comments: state.comments,
+	};
+};
+
+export default connect(mapStateToProps)(CommentList);
