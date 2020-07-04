003f15d6f3361685c54d39f674386aecf7622244
witoldmetel <witoldmetel@gmail.com>
2020-07-04 10:45:06 +0200

    update old tests

--- UPDATE_2020/testing/src/components/__test__/CommentBox.test.js
+++ UPDATE_2020/testing/src/components/__test__/CommentBox.test.js
@@ -18,9 +18,9 @@ afterEach(() => {
 	wrapped.unmount();
 });
 
-it('has a text area and button', () => {
+it('has a text area and 2 buttons', () => {
 	expect(wrapped.find('textarea').length).toEqual(1);
-	expect(wrapped.find('button').length).toEqual(1);
+	expect(wrapped.find('button').length).toEqual(2);
 });
 
 describe('the text area', () => {
--- UPDATE_2020/testing/src/components/__test__/Root.js
+++ UPDATE_2020/testing/src/components/__test__/Root.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
 
 import reducers from 'reducers';
 
