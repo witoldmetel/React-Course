3498d0590114de52d090095007d31d5520565bbc
witoldmetel <witoldmetel@gmail.com>
2020-10-03 14:56:19 +0200

    finish HOC

--- UPDATE_2020/testing/src/components/CommentBox.js
+++ UPDATE_2020/testing/src/components/CommentBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import * as actions from 'actions';
+import requreAuth from './requreAuth';
 
 class CommentBox extends Component {
 	state = {
@@ -38,4 +39,4 @@ class CommentBox extends Component {
 	}
 }
 
-export default connect(null, actions)(CommentBox);
+export default connect(null, actions)(requreAuth(CommentBox));
