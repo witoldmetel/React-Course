b18b13710b0d85b04361e1b46d4c781ee7560697
witoldmetel <witoldmetel@gmail.com>
2020-10-03 16:36:41 +0200

    add middlewares for node

---
+++ UPDATE_2020/auth/server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan'); // login framework mostly using for debugging
+
+// App setup
+const app = express();
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+
+server.listen(port);
+
+console.log('Server listening on', port);
