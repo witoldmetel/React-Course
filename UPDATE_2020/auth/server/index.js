1d7edb9009502341572cd6940c0a879f4126651c
witoldmetel <witoldmetel@gmail.com>
2020-10-03 16:43:17 +0200

    add router

--- UPDATE_2020/auth/server/index.js
+++ UPDATE_2020/auth/server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan'); // login framework mostly using for debugging
+const router = require('./router');
 
 // App setup
 const app = express();
@@ -12,6 +13,8 @@ app.use(bodyParser.json({ type: '*/*' }));
 // Server setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
+
+router(app);
 
 server.listen(port);
 
